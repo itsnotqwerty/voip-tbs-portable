c60a43fc4a98b116fa8d6f74f3d1ccbbac649bd6
Sam <samroux808@gmail.com>
2025-05-05 14:14:34 -0500

    Fixed logging timing

--- main.ts
+++ main.ts
@@ -107,7 +107,7 @@ Deno.serve(async (req) => {
         if (lastMessage) {
           const currentTime = Date.now() / 1000;
           const timeDifference = currentTime - lastMessage.unix_timestamp;
-          if (timeDifference > 30) { // 2.5 minutes in seconds
+          if (timeDifference > 150) { // 2.5 minutes in seconds
             // Archive the messages
             db.messages.archiveMessages(twilioNumber, callerNumber);
             logger.archiveMessages(callerNumber);
@@ -126,7 +126,7 @@ Deno.serve(async (req) => {
           }
         }
         resolve(new Response("Thank you for your messages. We will get back to you shortly.", {status: 200}));
-      }, 31000); // Slightly over 2.5 minutes
+      }, 151000); // Slightly over 2.5 minutes
     })
   } else {
     return new Response("Sorry we could not take your call. Please leave a message.", {status: 200});
